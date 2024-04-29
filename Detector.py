8470f5c6c657ba54438cc23a6de14504b7a5fb0b
jsonsini123 <jake.sonsini@menloschool.org>
2024-04-29 09:41:14 -0700

    multiple

--- Detector.py
+++ Detector.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np 
 
 # Read image. 
-img = cv2.imread('download.jpg', cv2.IMREAD_COLOR) 
+img = cv2.imread('lots.jpg', cv2.IMREAD_COLOR) 
 
 # Convert to grayscale. 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
@@ -11,23 +11,28 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray_blurred = cv2.blur(gray, (3, 3)) 
 
 # Apply Hough transform on the blurred image. 
-detected_circles = cv2.HoughCircles(gray_blurred, 
-				cv2.HOUGH_GRADIENT, 1, 20, param1 = 50, 
-			param2 = 30, minRadius = 1, maxRadius = 200) 
+detected_circles = cv2.HoughCircles(gray,
+                           cv2.HOUGH_GRADIENT,
+                           minDist=70,
+                           dp=1.1,
+                           param1=150,
+                           param2=95,
+                           minRadius=6,
+                           maxRadius=150)
 
 # Draw circles that are detected. 
 if detected_circles is not None: 
 
 	# Convert the circle parameters a, b and r to integers. 
 	detected_circles = np.uint16(np.around(detected_circles)) 
+	print(len(detected_circles))
 
 	for pt in detected_circles[0, :]: 
 		a, b, r = pt[0], pt[1], pt[2] 
 
 		# Draw the circumference of the circle. 
 		cv2.circle(img, (a, b), r, (0, 255, 0), 2) 
-
-		# Draw a small circle (of radius 1) to show the center. 
-		cv2.circle(img, (a, b), 1, (0, 0, 255), 3) 
-		cv2.imshow("Detected Circle", img) 
-		cv2.waitKey(0) 
+		
+	cv2.putText(img, "Number coins: " + str(len(detected_circles[0])), (20,20), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+	cv2.imshow("Detected Circle", img) 
+	cv2.waitKey(0) 
